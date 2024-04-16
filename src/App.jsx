6ed63104999a0c7273cc19c5a47150cfbd5b26fa
joat21 <eridel61@yandex.ru>
2024-04-16 15:57:53 +0500

    mapping categories

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ function App() {
             <PizzaBlock />
             <PizzaBlock />
             <PizzaBlock />
+            <PizzaBlock />
           </div>
         </div>
       </div>
--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,21 @@
+import { useState } from 'react';
+
 const Categories = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+
+  const onClickCategory = (index) => {
+    setActiveIndex(index);
+  };
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((category, i) => (
+          <li className={activeIndex === i ? 'active' : ''} onClick={() => onClickCategory(i)}>
+            {category}
+          </li>
+        ))}
       </ul>
     </div>
   );
